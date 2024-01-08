e763b3ea94a4fe734d71e8fbc3fb9da9ed29e51c
KaiHxx <jiikhk@gmail.com>
2024-01-07 23:35:06 -0800

    Added Three Buttons
    Home button redirects to Home page
    Edit Plans button redirects to AddPlan
    Add To Saved Plans button is a place holder

--- src/components/DisplayPlan.js
+++ src/components/DisplayPlan.js
@@ -1,11 +1,33 @@
+import { Button, Space } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function DisplayPlan() {
   console.log("display plan");
 
+  const addToSavedPlans = () => {
+    console.log("plan added!")
+  }
+
   return (
     <div>
-      <p>This is DisplayPlan page</p>
+      <div>
+        <Link to="/Home">
+          <Button>Home</Button>
+        </Link>
+      </div>
+      <Space>
+        <span>
+          <Link to="/AddPlan">
+            <Button onClick={addToSavedPlans}>Edit Plans</Button>
+          </Link>
+        </span>
+        <span>
+          <Button type="primary" onClick={addToSavedPlans}>
+            Add To Saved Plans
+          </Button>
+        </span>
+      </Space>
     </div>
   );
 }
