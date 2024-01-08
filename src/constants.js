026d030520e5dd3fb8bb66e99001a1b397a1585d
lqhu2020 <lqhu2020@gmail.com>
2024-01-08 11:53:12 -0800

    delete keys and backend api

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
 export const TOKEN_KEY = "token";
-export const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/http://34.82.85.194:8080/showDefaultPlaces?max_num_display=3";
-export const GOOGLE_MAP_API_KEY = "AIzaSyDcKFQbJJzSsVFHIz7ev1tXZh02TXL4wn8";
+export const BASE_URL = "";
+export const GOOGLE_MAP_API_KEY = "";
 export const CENTER_CITY = { lat: 40.73061, lng: -73.935242 };
 export const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 export const PLACE_NUM = 3;
